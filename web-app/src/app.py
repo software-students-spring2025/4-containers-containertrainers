15d056bcbe0b38baac69c542bf8362c36e47f069
nikitabhaskar <nikitab2204@gmail.com>
2025-04-07 16:22:53 -0400

    setting up web-app

---
+++ web-app/src/app.py
@@ -0,0 +1,56 @@
+import os
+import db
+from flask import Flask, render_template, request, redirect, abort, url_for, make_response, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.secret_key = os.urandom(12)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if not (username and password):
+            return render_template('index.html', message="All fields are required")
+        if db.accounts.find_one({username: username}):
+            return render_template('index.html', message="Account with this username already created.")
+        hashed_password = generate_password_hash(password)
+            
+        new_user = {
+            '_id': ObjectId(),
+            'username': username,
+            'password': hashed_password
+        }
+        db.accounts.insert_one(new_user)
+        return redirect(url_for('login'))
+    return render_template('register.html')
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = db.accounts.find_one({username: username})
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+        else:
+            return render_template('index.html', message="Invalid username or password")
+    return render_template('login.html')
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    PORT = os.getenv('PORT')
+    app.run(host='0.0.0.0', port=3000)
---
+++ web-app/src/db.py
@@ -0,0 +1,39 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv('MONGO_URI')
+MONGO_DBNAME = str(os.getenv('MONGO_DBNAME'))
+
+connection = MongoClient(MONGO_URI)
+db = connection[MONGO_DBNAME]
+
+acc_validator = {
+    "$jsonSchema": {
+        "bsonType": "object",
+        "required": ["username", "password"],
+        "properties": {
+            "username": {"bsonType": "string"},
+            "password": {"bsonType": "string"},
+            "messages": {"bsonType": "string"},
+        }
+    }
+}
+
+mess_validator = {
+    "$jsonSchema": {
+        "bsonType": "object",
+        "required": ["message"],
+        "properties": {
+            "message": {"bsonType": "string"},
+        }
+    }
+}
+
+accounts = db.accounts
+messages = db.messages
+
+# db.command('collMod', 'accounts', validator=acc_validator)
+# db.command('collMod', 'messages', validator=mess_validator)
