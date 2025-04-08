dbb92e3edd356e4ca25efd539e86e2ef480bb221
jg169 <jg6911@stern.nyu.edu>
2025-04-08 19:06:25 -0400

    added recording stuff

--- web-app/src/app.py
+++ web-app/src/app.py
@@ -1,7 +1,9 @@
 """App"""
 
 import os
-from flask import Flask, render_template, request, redirect, url_for, session
+import glob
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
 import db
@@ -73,6 +75,54 @@ def logout():
     """Logout Route"""
     session.clear()
     return render_template("index.html")
+
+
+#audio recording part
+RECORDINGS_FOLDER = '/app/recordings'
+os.makedirs(RECORDINGS_FOLDER, exist_ok=True)
+
+#look for the next file, file numbers are sequential
+def get_next_file_number():
+    """this gets the next file number"""
+    existing_files = glob.glob(os.path.join(RECORDINGS_FOLDER, "recording_*webm"))
+    #if no recordings
+    if not existing_files:
+        return 1
+    numbers = []
+    for filename in existing_files:
+        try:
+            base = os.path.basename(filename)
+            num_str = base.split('_')[1].split('.')[0]
+            numbers.append(int(num_str))
+        except (IndexError, ValueError):
+            continue
+
+    return max(numbers) + 1 if numbers else 1
+
+#main page
+@app.route('/record')
+def index():
+    ''' flask render the main page'''
+    return render_template('record.html')
+
+#uploads to the volume
+@app.route('/upload', methods=['POST'])
+def upload_audio():
+    '''see if the file is in the resquest get the file into the volume'''
+    if 'audio' not in request.files:
+        return jsonify({'success': False})
+
+    audio_file = request.files['audio']
+
+    next_number = get_next_file_number()
+
+    filename = f"recording_{next_number}.webm"
+
+    filepath = os.path.join(RECORDINGS_FOLDER, filename)
+    audio_file.save(filepath)
+
+    return jsonify({'success':True, 'filename': filename})
+
 
 
 if __name__ == "__main__":
