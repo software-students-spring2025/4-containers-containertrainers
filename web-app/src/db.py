98dbabeaf59cf4a82106862b3d966c449b656d1d
jg169 <jg6911@stern.nyu.edu>
2025-04-12 19:32:51 -0400

    is working for me?

--- web-app/src/db.py
+++ web-app/src/db.py
@@ -11,7 +11,7 @@ MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DBNAME = str(os.getenv("MONGO_DBNAME"))
 MONGO_DSN = os.getenv("MONGO_DSN")
 
-connection = MongoClient(MONGO_DSN, tlsCAFile=certifi.where())
+connection = MongoClient("mongodb://mongodb:27017")
 db = connection[MONGO_DBNAME]
 
 acc_validator = {
